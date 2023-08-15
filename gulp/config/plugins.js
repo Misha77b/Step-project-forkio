85f96022b01583b7b340b541178597b906ecf7ce
Misha <misha77b@gmail.com>
2023-08-15 23:04:56 +0300

    html gulp setup fine tuned

--- gulp/config/plugins.js
+++ gulp/config/plugins.js
@@ -1,5 +1,9 @@
-import replace from "gulp-replace";
+import replace from "gulp-replace"; // search and change
+import plumber from "gulp-plumber"; // error handler
+import notify from "gulp-notify"; // notification (hints)
 
 export const plugins = {
   replace: replace,
+  plumber: plumber,
+  notify: notify,
 };
