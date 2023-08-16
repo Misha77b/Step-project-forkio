55ee76d3b0beda5d7d7c02ec5225bd055ffbfad4
Misha <misha77b@gmail.com>
2023-08-16 11:24:34 +0300

    build all types of images amd minify

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -6,18 +6,22 @@ const srcFolder = "./src";
 
 export const path = {
   build: {
+    images: `${buildFolder}/images`,
     scripts: `${buildFolder}/scripts`,
     css: `${buildFolder}/styles`,
     html: `${buildFolder}`,
     // files: `${buildFolder}`,
   },
   src: {
+    images: `${srcFolder}/images/**/*.{jpg,jpeg,png,gif,webp}`,
     scripts: `${srcFolder}/scripts/script.js`,
+    svg: `${srcFolder}/images/**/*.svg`,
     scss: `${srcFolder}/styles/styles.scss`,
     html: `${srcFolder}/*.html`,
     // files: `${srcFolder}/**/*.*`,
   },
   watch: {
+    images: `${srcFolder}/images/**/*.{jpg,jpeg,png,svg,gif,ico,webp}`,
     scripts: `${srcFolder}/scripts/**/*.js`,
     scss: `${srcFolder}/styles/**/*.scss`,
     html: `${srcFolder}/**/*.html`,
--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ import { reset } from "./gulp/tasks/reset.js";
 import { server } from "./gulp/tasks/server.js";
 import { scss } from "./gulp/tasks/scss.js";
 import { scripts } from "./gulp/tasks/scripts.js";
+import { images } from "./gulp/tasks/images.js";
 // plugins
 import { plugins } from "./gulp/config/plugins.js";
 
@@ -22,9 +23,10 @@ function watcher() {
   gulp.watch(path.watch.html, html);
   gulp.watch(path.watch.scss, scss);
   gulp.watch(path.watch.scripts, scripts);
+  gulp.watch(path.watch.images, images);
 }
 //  main tasks
-const mainTask = gulp.parallel(html, scss, scripts);
+const mainTask = gulp.parallel(html, scss, scripts, images);
 // gulp task execution scenario
 const dev = gulp.series(reset, mainTask, gulp.parallel(watcher, server));
 // default task execution
